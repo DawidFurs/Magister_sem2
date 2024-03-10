2bcc594afcfee991a1f10f959f8cfb720b0602e3
DawidFurs <43808802+DawidFurs@users.noreply.github.com>
2024-03-10 14:26:54 +0100

    Zadanie 3

---
+++ zad_3.py
@@ -0,0 +1,16 @@
+'''3. Stworzyć funkcję, która będzie sprawdzała czy przekazana liczba w
+parametrze jest parzysta i zwróci tą informację jako typ logiczny bool
+( True / False ). Należy uruchomić funkcję, wynik wykonania zapisać do
+zmiennej, a następnie wykorzystując warunek logiczny wyświetlić prawidłowy
+tekst "Liczba parzysta" / "Liczba nieparzysta"'''
+
+def check_if_even(a: int) -> bool:
+    if a%2==0:
+        return True
+
+number = check_if_even(14)
+
+if number:
+    print("Liczba parzysta")
+else:
+    print("Liczba nieparzysta")
