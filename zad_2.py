2c8df4f76dc765bab576e536660581cf5d83a414
DawidFurs <43808802+DawidFurs@users.noreply.github.com>
2024-03-10 14:23:27 +0100

    Zadanie 2

---
+++ zad_2.py
@@ -0,0 +1,8 @@
+'''2. Stworzyć funkcję, która przyjmie 2 argumenty typu int , a następnie zwróci
+wynik mnożenia obu liczb.'''
+
+
+def multiplying(a: int, b: int) -> int:
+    return a*b
+
+print(multiplying(4,5))
