a75c1ce8a8ffe99f5cc1c11a0a9dba540a691bd4
DawidFurs <43808802+DawidFurs@users.noreply.github.com>
2024-03-10 14:21:25 +0100

    Zadanie pierwsze

---
+++ zad_1.py
@@ -0,0 +1,11 @@
+'''1. Stworzyć funkcję, która przyjmuje 2 argumenty (typu string ) name i surname , a
+następnie zwróci stringa zgodnie ze wzorem Cześć {name} {surname}! Należy
+uruchomić funkcję, wynik wykonania funkcji zapisać do zmiennej, a następnie
+go wyświetlić ( print )'''
+
+def say_hello(name: str, surname: str) -> str:
+    return(f"Cześć {name} {surname}!")
+
+przywitanie = say_hello("Dawid", "Furs")
+
+print(przywitanie)
